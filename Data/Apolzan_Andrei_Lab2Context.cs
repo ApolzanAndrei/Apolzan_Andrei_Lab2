e519db5814b204ba99945bb314bbd3722ef0cc09
apolz <apolz@DESKTOP-TCC6PEF>
2022-10-30 22:08:11 +0200

    Add project files.

---
+++ Data/Apolzan_Andrei_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Apolzan_Andrei_Lab2.Models;
+
+namespace Apolzan_Andrei_Lab2.Data
+{
+    public class Apolzan_Andrei_Lab2Context : DbContext
+    {
+        public Apolzan_Andrei_Lab2Context (DbContextOptions<Apolzan_Andrei_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Apolzan_Andrei_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Apolzan_Andrei_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Apolzan_Andrei_Lab2.Data;
+using Apolzan_Andrei_Lab2.Models;
+
+namespace Apolzan_Andrei_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Apolzan_Andrei_Lab2.Data.Apolzan_Andrei_Lab2Context _context;
+
+        public IndexModel(Apolzan_Andrei_Lab2.Data.Apolzan_Andrei_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
